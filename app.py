ed5d8ee5aed3805998a747d265dda4c82553c160
ZHANG WEIYI <wzhangel@connect.ust.hk>
2026-05-10 13:19:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,7 @@ def img2text(url):
 def text2story(text):
     story_pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
     prompt = "Write a short story for children based on: " + text
-    story_results = story_pipe(
-        prompt,
-        max_length=120,
-        min_length=60,
-        do_sample=True,
-        temperature=0.9,
-        repetition_penalty=1.5,
-        no_repeat_ngram_size=3
-    )
+    story_results = story_pipe(prompt, max_length=120, min_length=60, do_sample=True, temperature=0.9, repetition_penalty=1.5, no_repeat_ngram_size=3)
     story_text = story_results[0]["generated_text"]
     return story_text
 
@@ -33,43 +25,33 @@ def text2audio(story_text):
 # Main part
 st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
 st.header("Turn Your Image to Audio Story")
-
 uploaded_file = st.file_uploader("Select an Image...")
 
 if uploaded_file is not None:
-
     # Save file locally
     bytes_data = uploaded_file.getvalue()
-
     with open(uploaded_file.name, "wb") as file:
         file.write(bytes_data)
 
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-    # Stage 1: Image to Text
-    st.text('Processing img2text...')
+    # Generate button
+    if st.button("Generate Story"):
+        # Stage 1: Image to Text
+        st.text('Processing img2text...')
+        scenario = img2text(uploaded_file.name)
+        st.write(f"**Scenario:** {scenario}")
 
-    scenario = img2text(uploaded_file.name)
+        # Stage 2: Text to Story
+        st.text('Generating a story...')
+        story = text2story(scenario)
+        st.write(f"**Story:** {story}")
 
-    st.write(f"**Scenario:** {scenario}")
+        # Stage 3: Story to Audio
+        st.text('Generating audio data...')
+        audio_data = text2audio(story)
 
-    # Stage 2: Text to Story
-    st.text('Generating a story...')
-
-    story = text2story(scenario)
-
-    st.write(f"**Story:** {story}")
-
-    # Stage 3: Story to Audio
-    st.text('Generating audio data...')
-
-    audio_data = text2audio(story)
-
-    # Play button
-    if st.button("Play Audio"):
-
+        # Play Audio
         audio_array = audio_data["audio"]
-
         sample_rate = audio_data["sampling_rate"]
-
         st.audio(audio_array, sample_rate=sample_rate)
