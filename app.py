5610d683bf6c3c72f9e2d5fc3fa8ef78ff771aa8
ZHANG WEIYI <wzhangel@connect.ust.hk>
2026-05-10 13:48:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,59 +2,44 @@ from transformers import pipeline
 import streamlit as st
 
 # function part
+# img2text
 def img2text(url):
     image_to_text_model = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base")
     text = image_to_text_model(url)[0]["generated_text"]
     return text
 
+# text2story
 def text2story(text):
-    story_pipe = pipeline("text-generation", model="pranavpsv/genre-story-generator-v2")
-    prompt = "Write a short story for children based on: " + text
-    story_results = story_pipe(
-        prompt,
-        max_length=120,
-        min_length=60,
-        do_sample=True,
-        temperature=0.9,
-        repetition_penalty=1.5,
-        no_repeat_ngram_size=3
-    )
-    story_text = story_results[0]["generated_text"]
+    story_generator = pipeline("text2text-generation", model="google/flan-t5-base")
+    prompt = "Write a short, simple, happy story for children aged 3 to 10 in 50 to 100 words. Only output the story. The story is based on this image description: " + text
+    story = story_generator(prompt, max_new_tokens=120)
+    story_text = story[0]["generated_text"]
     return story_text
 
+# text2audio
 def text2audio(story_text):
-    audio_pipe = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
-    audio_data = audio_pipe(story_text)
+    audio_generator = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
+    audio_data = audio_generator(story_text)
     return audio_data
 
 # main part
 st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
-
 st.header("Turn Your Image to Audio Story")
-
 uploaded_file = st.file_uploader("Select an Image...")
 
 if uploaded_file is not None:
-
     bytes_data = uploaded_file.getvalue()
-
     with open(uploaded_file.name, "wb") as file:
         file.write(bytes_data)
 
     st.image(uploaded_file, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Generate Story"):
-
         scenario = img2text(uploaded_file.name)
-
         story = text2story(scenario)
-
         st.write(story)
 
         audio_data = text2audio(story)
-
         audio_array = audio_data["audio"]
-
         sample_rate = audio_data["sampling_rate"]
-
         st.audio(audio_array, sample_rate=sample_rate)
