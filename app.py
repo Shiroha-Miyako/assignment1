800d995d992941a00319a67e16e02a3996916d91
ZHANG WEIYI <wzhangel@connect.ust.hk>
2026-05-10 13:20:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ def text2audio(story_text):
 # Main part
 st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
 st.header("Turn Your Image to Audio Story")
+
 uploaded_file = st.file_uploader("Select an Image...")
 
 if uploaded_file is not None:
     # Save file locally
     bytes_data = uploaded_file.getvalue()
+
     with open(uploaded_file.name, "wb") as file:
         file.write(bytes_data)
 
@@ -37,21 +39,21 @@ if uploaded_file is not None:
 
     # Generate button
     if st.button("Generate Story"):
+
         # Stage 1: Image to Text
-        st.text('Processing img2text...')
         scenario = img2text(uploaded_file.name)
-        st.write(f"**Scenario:** {scenario}")
 
         # Stage 2: Text to Story
-        st.text('Generating a story...')
         story = text2story(scenario)
-        st.write(f"**Story:** {story}")
+
+        st.write(story)
 
         # Stage 3: Story to Audio
-        st.text('Generating audio data...')
         audio_data = text2audio(story)
 
         # Play Audio
         audio_array = audio_data["audio"]
+
         sample_rate = audio_data["sampling_rate"]
+
         st.audio(audio_array, sample_rate=sample_rate)
